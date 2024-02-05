d8ade75fceefa025e1c02eabbad6b28a4f998c64
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2024-02-05 16:26:59 +0330

    Fixed createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import About from "../views/About";
 import Home from "../views/Home";
@@ -17,7 +17,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
