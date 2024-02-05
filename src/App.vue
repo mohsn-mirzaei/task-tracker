11ecfb99dd646f206a6fcde31d9692e3518bf196
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2024-02-05 13:42:52 +0330

    Implement Emit Event (Toggle Reminder)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks @delete-task="deleteTask" :tasks="tasks" />
+    <Tasks
+      @delete-task="deleteTask"
+      @toggle-reminder="toggleReminder"
+      :tasks="tasks"
+    />
   </div>
 </template>
 
@@ -25,6 +29,11 @@ export default {
       if (confirm("Are you sure?")) {
         this.tasks = this.tasks.filter((task) => task.id !== id);
       }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
     },
   },
   created() {
